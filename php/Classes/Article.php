363618c1d6ccb80142a319ec643582b1766ab3d0
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-10-28 20:00:24 -0600

    PDO Statements completed

--- php/Classes/Article.php
+++ php/Classes/Article.php
@@ -237,3 +237,238 @@ public function setArticleTitle( $newArticleTitle) : void {
 }
 };
 
+/*
+PDO
+starts
+here:
+*/
+
+public function insert(\PDO $pdo) : void {
+
+	$query = "INSERT INTO Article(articleAuthorId, articleId, articleFilterId, articleDatePublished,
+articleDescription, articleTitle) VALUES(:articleAuthorId, :articleId, :articleFilterId, :articleDatePublished,
+:articleDescription, :articleTitle)";
+	$statement = $pdo->prepare($query);
+
+// bind the member variables to the place holders in the template
+	$parameters = ["articleAuthorId" => $this->articleAuthorId->getBytes(), "articleId" => $this->articleId->getBytes(),
+		"articleFilterId" => $this->articleFilterId, "articleDatePublished" => $this->articleDatePublished,
+		"articleDescription" => $this->articleDescription, "articleTitle" => $this->articleTitle];
+
+	$statement->execute($parameters);
+}
+
+/**
+ * deletes this Article from mySQL
+ *
+ * @param \PDO $pdo PDO connection object
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError if $pdo is not a PDO connection object
+ **/
+public function delete(\PDO $pdo) : void {
+
+// create query template
+	$query = "DELETE FROM article WHERE articleAuthorId = :articleAuthorId";
+	$statement = $pdo->prepare($query);
+
+// bind the member variables to the place holder in the template
+	$parameters = ["articleAuthorId" => $this->articleAuthorId->getBytes()];
+	$statement->execute($parameters);
+}
+
+/**
+ * updates this Article in mySQL
+ *
+ * @param \PDO $pdo PDO connection object
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError if $pdo is not a PDO connection object
+ **/
+public function update(\PDO $pdo) : void {
+
+// create query template
+	$query = "UPDATE Article SET articleAuthorId = :articleAuthorId, articleId = :articleId, articleFilterId = :articleFilterId,
+articleDatePublished = :articleDatePublished, articleDescription = :articleDescription, articleTitle = :articleTitle";
+	$statement = $pdo->prepare($query);
+
+
+	$parameters = ["articleAuthorId" => $this->articleAuthorId->getBytes(),"articleId" => $this->articleId->getBytes(),
+		"articleFilterId" => $this->articleFilterId->getBytes(), "articleDatePublished" => $this->articleDatePublished->getBytes(),
+		"articleDescription" => $this->articleDescription->getBytes(), "articleTitle" => $this->articleTitle->getBytes();]
+
+	
+$statement->execute($parameters);
+};
+
+/**
+* gets the Article by articleId
+*
+* @param \PDO $pdo PDO connection object
+* @param Uuid|string $articleId article id to search for
+* @return Article|null Article found or null if not found
+* @throws \PDOException when mySQL related errors occur
+* @throws \TypeError when a variable are not the correct data type
+**/
+public static function getArticleByArticleId(\PDO $pdo, $articleId) : ?Article {
+// sanitize the articleId before searching
+try {
+$articleId = self::validateUuid($articleId);
+} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+throw(new \PDOException($exception->getMessage(), 0, $exception));
+}
+
+// create query template
+$query = "SELECT articleId, articleAuthorId, articleFilterId, articleDatePublished, articleDescription, articleTitle FROM article WHERE articleId = :articleId";
+$statement = $pdo->prepare($query);
+
+// bind the tweet id to the place holder in the template
+$parameters = ["articleId" => $articleId->getBytes()];
+$statement->execute($parameters);
+
+// grab the tweet from mySQL
+try {
+$article = null;
+$statement->setFetchMode(\PDO::FETCH_ASSOC);
+$row = $statement->fetch();
+if($row !== false) {
+$article = new Tweet($row["articleId"], $row["articleAuthorId"], $row["articleFilterId"],
+	$row["articleDatePublished", $row["articleDescription"], $row["articleTitle"]);
+}
+} catch(\Exception $exception) {
+// if the row couldn't be converted, rethrow it
+throw(new \PDOException($exception->getMessage(), 0, $exception));
+}
+return($article);
+}
+
+/**
+* gets the Article by article author id
+*
+* @param \PDO $pdo PDO connection object
+* @param Uuid| $articleAuthorId author id to search by
+* @return \SplFixedArray SplFixedArray of Articles found
+* @throws \PDOException when mySQL related errors occur
+* @throws \TypeError when variables are not the correct data type
+**/
+public static function getArticleByArticleAuthorId(\PDO $pdo, $articleAuthorId) : \SplFixedArray {
+
+try {
+$articleAuthorId = self::validateUuid($articleAuthorId);
+} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+throw(new \PDOException($exception->getMessage(), 0, $exception));
+}
+
+// create query template
+$query = "SELECT articleId, articleAuthorId, articleFilterId, articleDatePublished, articleDescription, articleTitle FROM article WHERE articleAuthorId = :articleAuthorId";
+$statement = $pdo->prepare($query);
+// bind the tweet profile id to the place holder in the template
+$parameters = ["articleAuthorId" => $articleAuthorId->getBytes()];
+$statement->execute($parameters);
+// build an array of tweets
+$article = new \SplFixedArray($statement->rowCount());
+$statement->setFetchMode(\PDO::FETCH_ASSOC);
+while(($row = $statement->fetch()) !== false) {
+try {
+$articles = new Article($row["articleId"], $row["articleAuthorId"], $row["articleFilterId"], $row["articleDatePublished"],
+	$row["articleDescription"], $row["articleTitle"]);
+$articles[$articles->key()] = $article;
+$articles->next();
+} catch(\Exception $exception) {
+// if the row couldn't be converted, rethrow it
+throw(new \PDOException($exception->getMessage(), 0, $exception));
+}
+}
+return($article);
+}
+
+/**
+* gets the Article by article description
+ *
+* @param \PDO $pdo PDO connection object
+* @param string $articleDescription to search for
+* @return \SplFixedArray SplFixedArray of Articles found
+* @throws \PDOException when mySQL related errors occur
+* @throws \TypeError when variables are not the correct data type
+**/
+public static function getArticleByArticleDescription(\PDO $pdo, string $articleDescription) : \SplFixedArray {
+// sanitize the description before searching
+$articleDescription = trim($articleDescription);
+$articleDescription = filter_var($articleDescription, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+if(empty($articleDescription) === true) {
+throw(new \PDOException("article description is invalid"));
+}
+
+// escape any mySQL wild cards
+$articleDescription = str_replace("_", "\\_", str_replace("%", "\\%", $articleDescription));
+
+// create query template
+$query = "SELECT articleId, articleAuthorId, articleDescription, articleFilterId, articleDatePublished, articleTitle 
+FROM article WHERE articleDescription LIKE :articleDescription";
+$statement = $pdo->prepare($query);
+
+// bind the tweet content to the place holder in the template
+$articleDescription = "%$articleDescription%";
+$parameters = ["articleDescription" => $articleDescription];
+$statement->execute($parameters);
+
+// build an array of tweets
+$articles = new \SplFixedArray($statement->rowCount());
+$statement->setFetchMode(\PDO::FETCH_ASSOC);
+while(($row = $statement->fetch()) !== false) {
+try {
+$article = new Article($row["articleId"], $row["articleAuthorId"], $row["articleDescription"], $row["articleFilterId"],
+	$row["articleDatePublished"], $row["articleTitle"]);
+$article[$articles->key()] = $article;
+$articles->next();
+} catch(\Exception $exception) {
+// if the row couldn't be converted, rethrow it
+throw(new \PDOException($exception->getMessage(), 0, $exception));
+}
+}
+return($articles);
+}
+
+/**
+* gets all Articles
+*
+* @param \PDO $pdo PDO connection object
+* @return \SplFixedArray SplFixedArray of Tweets found or null if not found
+* @throws \PDOException when mySQL related errors occur
+* @throws \TypeError when variables are not the correct data type
+**/
+public static function getAllArticles(\PDO $pdo) : \SPLFixedArray {
+// create query template
+$query = "SELECT articleId, articleAuthorId, articleDescription, articleFilterId, articleDatePublished, articleTitle FROM article";
+$statement = $pdo->prepare($query);
+$statement->execute();
+
+// build an array of tweets
+$articles = new \SplFixedArray($statement->rowCount());
+$statement->setFetchMode(\PDO::FETCH_ASSOC);
+while(($row = $statement->fetch()) !== false) {
+try {
+$article = new Article($row["articleId"], $row["articleAuthorId"], $row["articleDescription"], $row["articleFilterId"],
+	$row["articleDatePublished"], $row["articleTitle"]);
+$articles[$articles->key()] = $article;
+$articles->next();
+} catch(\Exception $exception) {
+// if the row couldn't be converted, rethrow it
+throw(new \PDOException($exception->getMessage(), 0, $exception));
+}
+}
+return ($articles);
+}
+
+/**
+* formats the state variables for JSON serialization
+*
+* @return array resulting state variables to serialize
+**/
+public function jsonSerialize() : array {
+$fields = get_object_vars($this);
+
+$fields["articleId"] = $this->articleId->toString();
+$fields["articleAuthorId"] = $this->articleAuthorId->toString();
+
+return($fields);
+}
+};
