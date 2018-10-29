e7e1d1d9e48706d297748f16469de69d871c9f8d
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-10-28 18:35:18 -0600

    PDO insert into: done with error

--- php/lib/article.php
+++ php/lib/article.php
@@ -4,8 +4,9 @@ require_once("../Classes/Article.php");
 
 $date = new \DateTime('now');
 //the constructor is executed here
-$Article = new Article("e08e550f-a39d-47c0-acac-0d23958647fe", "7373b074-6763-4500-86f6-ae1bd93cb4f7", "7373b074-6763-4500-86f6-ae1bd93cb4f7",
-	$date, "This article shows the reader how to live in Tonga", "How to live in Tonga");
+$Article = new Article("e08e550f-a39d-47c0-acac-0d23958647fe", "7373b074-6763-4500-86f6-ae1bd93cb4f7",
+	"7373b074-6763-4500-86f6-ae1bd93cb4f7", $date, "This article shows the reader how to live in Tonga",
+	"How to live in Tonga");
 
 var_dump($Article);
 
